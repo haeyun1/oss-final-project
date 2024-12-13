472717c8c0f630e4d80b3ecef1ef79e7b134d3c3
haeyun1 <jhy041020@handong.ac.kr>
2024-12-13 19:10:27 +0900

    asdf

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,8 @@ function Navbar() {
                 <li>
                     <Link to="/">Home</Link> {/* 홈으로 이동하는 링크 */}
                 </li>
+                <img className="nav-img" src="https://blog.kakaocdn.net/dn/bwbgz6/btsFFXP9Kub/lOQHfqPuNvcxRes1ZAY2Yk/img.png"/>
+
                 <li>
                     <Link to="/search">Search</Link> {/* 검색 페이지로 이동하는 링크 */}
                 </li>
