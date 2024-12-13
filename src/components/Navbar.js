21529d5221ba6fa8209a91bbf6cafbed7c7cab00
yjun02 <jyjoons@gmail.com>
2024-12-13 20:19:54 +0900

    add alt

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom"; // Link 컴포넌트를 import 합니다.
 
 function Navbar() {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link> {/* 홈으로 이동하는 링크 */}
-                </li>
-                <img className="nav-img" src="https://blog.kakaocdn.net/dn/bwbgz6/btsFFXP9Kub/lOQHfqPuNvcxRes1ZAY2Yk/img.png"/>
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link> {/* 홈으로 이동하는 링크 */}
+        </li>
+        <img className="nav-img" src="https://blog.kakaocdn.net/dn/bwbgz6/btsFFXP9Kub/lOQHfqPuNvcxRes1ZAY2Yk/img.png" alt="maplestoryLogo" />
 
-                <li>
-                    <Link to="/search">Search</Link> {/* 검색 페이지로 이동하는 링크 */}
-                </li>
-            </ul>
-        </nav>
-    );
+        <li>
+          <Link to="/search">Search</Link> {/* 검색 페이지로 이동하는 링크 */}
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
